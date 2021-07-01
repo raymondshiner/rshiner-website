3b775f711777fe60b00f19c435a1a63b4a269f05
Raymond Shiner <raymondshiner@gmail.com>
2021-07-01 13:26:46 -0700

    placeholder website done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import "./App.css";
-import logo from "./logo.svg";
+import githubLogo from "./assets/githubLogo.svg";
+import linkedInLogo from "./assets/linkedInLogo.svg";
+import reactLogo from "./assets/reactLogo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>This is a test</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img src={reactLogo} className="App-logo" alt="logo" />
+          <h3>Raymond Shiner's Website</h3>
+        </div>
+        <h5 style={{ maxWidth: "500px", width: "80vw", marginBottom: 50 }}>
+          Hi, I'm currently working on redesigning this website, please check
+          back later
+        </h5>
+        <p style={{ marginBottom: 40 }}>Here are some of my links!</p>
+        <section
+          id="links"
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            minWidth: "400px",
+            width: "35vw",
+          }}
         >
-          Learn React
-        </a>
+          <a
+            className="App-link"
+            href="https://github.com/raymondshiner"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubLogo} alt="github" style={{ height: "8vmin" }} />
+          </a>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/raymond-shiner/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedInLogo} alt="github" style={{ height: "8vmin" }} />
+          </a>
+        </section>
       </header>
     </div>
   );
