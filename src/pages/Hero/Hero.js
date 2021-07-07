dca61dc738ecfb1e6be683d260b2c83387ae2667
Raymond Shiner <raymondshiner@gmail.com>
2021-07-06 20:13:43 -0700

    add particles

--- src/pages/Hero/Hero.js
+++ src/pages/Hero/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Particles from "react-particles-js";
 import styled from "styled-components";
 import SocialLinks from "./SocialLinks";
 import TerminalAnimation from "./TerminalAnimation";
@@ -6,18 +7,34 @@ import TerminalAnimation from "./TerminalAnimation";
 const Hero = () => {
   return (
     <>
-      <FlexBoxColumn>
-        <MainText>Raymond Shiner</MainText>
-        <TerminalAnimation />
-        <SocialLinks />
-      </FlexBoxColumn>
+      <HeroWrapper>
+        <StyledParticles />
+        <FlexBoxColumn>
+          <MainText>Raymond Shiner</MainText>
+          <TerminalAnimation />
+          <SocialLinks />
+        </FlexBoxColumn>
+      </HeroWrapper>
     </>
   );
 };
 
 export default Hero;
 
+const HeroWrapper = styled.div`
+  position: relative;
+`;
+
+const StyledParticles = styled(Particles)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
 const FlexBoxColumn = styled.div`
+  position: relative;
   height: 100%;
   margin-top: 90px;
   flex-wrap: nowrap;
@@ -28,6 +45,7 @@ const FlexBoxColumn = styled.div`
 `;
 
 const MainText = styled.h1`
+  background-color: black;
   font-size: clamp(3.3rem, 10vw, 6.5rem);
   width: fit-content;
   min-width: 300px;
