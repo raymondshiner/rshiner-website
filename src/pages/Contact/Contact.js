f5d1237a561b898819ad2ab8ac46f9206ed8f2c7
Raymond Shiner <raymondshiner@gmail.com>
2021-07-15 16:34:29 -0700

    contact visuals done

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,134 +1,107 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Input from "./Input";
+import TextBoxInput from "./TextBoxInput";
 
 const Contact = () => {
   const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const allFormsFilled = name && email && subject && message;
+
   const sendEmail = () => {};
 
   return (
-    <ContactWrapper>
+    <Form onSubmit={sendEmail}>
       <Title>Contact Me</Title>
-      <Form onSubmit={sendEmail}>
-        <InputContainer>
-          <Input
-            placeholder=""
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Label>Name</Label>
-        </InputContainer>
-      </Form>
-    </ContactWrapper>
+      <Input
+        label="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        label="Email address"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        label="Subject"
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
+      />
+      <TextBoxInput
+        label="Your mesage"
+        onChange={(e) => setMessage(e.target.value)}
+        disabled={!true}
+      />
+      <SubmitButton disabled={!allFormsFilled} />
+    </Form>
   );
 };
 
 export default Contact;
 
 const ContactWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   background: black;
   margin: 50px;
   padding: 40px;
   width: 100%;
-  max-width: 600px;
+  max-width: 500px;
   border-radius: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Title = styled.h1`
   color: ${(props) => props.theme.primary};
+  font-size: 40px;
   white-space: nowrap;
   margin-bottom: 10px;
 `;
 
 const Form = styled.form`
+  background: black;
+  margin: 50px;
+  padding: 40px;
   width: 100%;
-`;
-
-const Divider = styled.div`
-  height: 5px;
-  background-color: ${(props) => props.theme.backgroundHighlight};
-  margin: 10px 0px;
-  border-radius: 15px;
-`;
-
-const Label = styled.label.attrs({
-  className: "label",
-})`
-  position: absolute;
-  pointer-events: none;
-  left: 20px;
-  top: 37px;
-  font-size: 14px;
-  color: #808080;
-  transition: all ease 0.5s;
-`;
-
-const InputContainer = styled.div`
+  max-width: 500px;
+  border-radius: 50px;
   display: flex;
-  position: relative;
-  height: 90px;
-  width: inherit;
-  padding-left: 5px;
+  flex-direction: column;
   align-items: center;
-  border: 1px solid red;
 `;
 
-const Input = styled.input.attrs({
-  type: "text",
-  required: true,
+const SubmitButton = styled.input.attrs({
+  type: "submit",
+  value: "submit",
 })`
-  height: 55px;
-  width: inherit;
-  font-size: 14px;
-  caret-color: ${(props) => props.theme.secondaryText};
-  padding-left: 10px;
-  border-radius: 4px;
-  border-style: solid;
-  color: white;
-  border-width: 0.5px;
-  border-color: black;
-  background-color: ${(props) => props.theme.backgroundHighlight};
-  transition: border-color 0.3s;
+  width: 100px;
+  height: 40px;
+  border-radius: 20px;
+  border: none;
+  letter-spacing: 1px;
+  font-size: 15px;
+  text-transform: uppercase;
+  transition: all ease 0.5s;
+  color: black;
+  background-color: ${(props) => props.theme.primary};
+  cursor: pointer;
 
   :hover {
-    /* border-color: ${(props) => props.theme.secondaryText}; */
-    border-color: #808080;
+    transform: translateY(-3px);
   }
 
-  :focus {
-    padding-left: 9px;
-    border-width: 2px;
-    border-color: ${(props) => props.theme.secondaryText};
-    outline: none;
+  :active {
+    transform: translateY(1px);
+    transition: none;
   }
 
-  :focus + .label {
-    top: 0px;
-    left: 10px;
-    font-size: 12px;
-    color: ${(props) => props.theme.secondaryText};
-    padding: 5px;
-    padding-top: 2px;
-  }
-
-  :valid {
-    top: 2px;
-    left: 10px;
-    font-size: 12px;
+  :disabled {
     background-color: ${(props) => props.theme.backgroundHighlight};
-    padding-left: 10px;
-    padding-right: 5px;
-  }
-
-  :valid + .label {
-    top: 0px;
-    left: 10px;
-    font-size: 12px;
-    color: ${(props) => props.theme.secondaryText};
-    border-radius: 15px;
-    padding: 5px;
-    padding-top: 2px;
+    cursor: none;
   }
 `;
---
+++ src/pages/Contact/Input.js
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+
+const Input = ({ label, ...props }) => {
+  return (
+    <InputContainer>
+      <StyledInput {...props} />
+      <Label>{label}</Label>
+    </InputContainer>
+  );
+};
+
+export default Input;
+
+const InputContainer = styled.div`
+  display: flex;
+  position: relative;
+  height: 90px;
+  width: inherit;
+  padding: 0px 5px;
+  align-items: center;
+`;
+
+const StyledInput = styled.input.attrs({
+  type: "text",
+  required: true,
+})`
+  height: 55px;
+  width: inherit;
+  font-size: 14px;
+  caret-color: ${(props) => props.theme.secondaryText};
+  padding-left: 10px;
+  border-radius: 4px;
+  border-style: solid;
+  color: white;
+  border-width: 0.5px;
+  border-color: black;
+  background-color: ${(props) => props.theme.backgroundHighlight};
+  transition: border-color 0.3s;
+
+  :hover {
+    /* border-color: ${(props) => props.theme.secondaryText}; */
+    border-color: #808080;
+  }
+
+  :focus {
+    padding-left: 9px;
+    border-width: 2px;
+    border-color: ${(props) => props.theme.secondaryText};
+    outline: none;
+  }
+
+  :focus + .label {
+    top: 0px;
+    left: 10px;
+    font-size: 12px;
+    color: ${(props) => props.theme.secondaryText};
+    padding: 5px;
+    padding-top: 2px;
+  }
+
+  :valid {
+    top: 2px;
+    left: 10px;
+    background-color: ${(props) => props.theme.backgroundHighlight};
+    padding-left: 10px;
+    padding-right: 5px;
+  }
+
+  :valid + .label {
+    top: 0px;
+    left: 10px;
+    font-size: 12px;
+    color: ${(props) => props.theme.secondaryText};
+    border-radius: 15px;
+    padding: 5px;
+    padding-top: 2px;
+  }
+`;
+
+const Label = styled.label.attrs({
+  className: "label",
+})`
+  position: absolute;
+  pointer-events: none;
+  left: 20px;
+  top: 37px;
+  font-size: 14px;
+  color: #808080;
+  transition: all ease 0.5s;
+`;
